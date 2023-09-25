1c107d9bd567ddf855ded91bea24401cb1869987
Göktuğ Aşcı <g.asci@hotmail.com>
2023-09-25 12:22:12 +0200

    add target dev support

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@ source .venv/bin/activate
 source .env
 
 MODEL=${1:-all}
+ENV=${2:-prod}
 
 export DBT_HOST=$DBT_HOST
 export DBT_USERNAME=$DBT_USERNAME
@@ -10,14 +11,14 @@ export DBT_DB_NAME=$DBT_DB_NAME
 
 if [ $MODEL == "all" ]
 then
-    dbt run --select tag:annotation_input+ --target prod 
-    dbt run --select tag:annotation_output+ --threads 1 --full-refresh --target prod 
+    dbt run --select tag:annotation_input+ --target $ENV 
+    dbt run --select tag:annotation_output+ --threads 1 --full-refresh --target $ENV 
 elif [ $MODEL == "docs" ]
 then
     dbt docs generate
     dbt docs serve
 else
-    dbt run --select $MODEL --target prod 
+    dbt run --select $MODEL --target $ENV 
 fi
 
 deactivate
