2a4e7f3c4420f9aac42f882d00526521bac01866
gasci <g.asci@hotmail.com>
2023-10-08 19:57:12 +0200

    add

--- .archive/database.sql
+++ .archive/database.sql
@@ -1,18 +1,23 @@
--- create a new role
-CREATE ROLE read_only_all;
-
-GRANT mle TO oytun_demirbilek;
-
-GRANT SELECT ON ALL TABLES IN SCHEMA public 
-TO read_only_all;
-
-GRANT INSERT, UPDATE, SELECT, DELETE
-ON ALL TABLES IN SCHEMA public 
-TO read_write_all;
-
 -- create a new user
 CREATE USER oytun_demirbilek WITH login password 'change_me_2413' 
 nosuperuser inherit nocreatedb nocreaterole noreplication valid until 'infinity' 
 IN GROUP read_only_all;
 
-ALTER USER oytun_demirbilek WITH password 'your_new_password_with_quotes';
+ALTER USER hamza_guzel WITH password '1992h1649';
+
+-- create a new role
+-- CREATE ROLE read_only_all;
+GRANT mle TO oytun_demirbilek;
+
+GRANT
+SELECT
+    ON ALL TABLES IN SCHEMA public TO read_only_all;
+
+GRANT
+INSERT
+,
+UPDATE
+,
+SELECT
+,
+    DELETE ON ALL TABLES IN SCHEMA public TO read_write_all;
